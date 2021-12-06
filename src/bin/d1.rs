14ae62e239cdb10853f7320039e94713c3e22343
dwat3r <hechtloliver@gmail.com>
2021-12-06 10:55:26 +0100

    format

--- src/bin/d1.rs
+++ src/bin/d1.rs
@@ -1,34 +1,34 @@
 use std::fs;
 
 fn main() {
-    let f = fs::read_to_string("d1.txt").expect("no file");
-    let v = f
-        .split_whitespace()
-        .map(|x| x.parse::<i32>().expect("number expected"));
-    let mut count = -1;
-    let mut prev = -1;
-    v.for_each(|x| {
-        if prev < x {
-            count += 1;
-        }
-        prev = x;
-    });
-    let part1 = count;
-    println!("{}", part1);
-
-    count = 0;
-    let mut window = Vec::new();
-    for x in f.split_whitespace() {
-        let x: i32 = x.parse().unwrap();
-        window.push(x);
-        if window.len() == 4 {
-            let first: i32 = window[0..3].iter().sum();
-            let second: i32 = window[1..4].iter().sum();
-            if first < second {
-                count += 1;
-            }
-            window.remove(0);
-        }
+  let f = fs::read_to_string("d1.txt").expect("no file");
+  let v = f
+    .split_whitespace()
+    .map(|x| x.parse::<i32>().expect("number expected"));
+  let mut count = -1;
+  let mut prev = -1;
+  v.for_each(|x| {
+    if prev < x {
+      count += 1;
     }
-    println!("{}", count);
+    prev = x;
+  });
+  let part1 = count;
+  println!("{}", part1);
+
+  count = 0;
+  let mut window = Vec::new();
+  for x in f.split_whitespace() {
+    let x: i32 = x.parse().unwrap();
+    window.push(x);
+    if window.len() == 4 {
+      let first: i32 = window[0..3].iter().sum();
+      let second: i32 = window[1..4].iter().sum();
+      if first < second {
+        count += 1;
+      }
+      window.remove(0);
+    }
+  }
+  println!("{}", count);
 }
--- src/bin/d6.rs
+++ src/bin/d6.rs
@@ -1,59 +1,59 @@
 use std::{collections::HashMap, fs};
 
 fn main() {
-    let f = fs::read_to_string("d6.txt")
-        .expect("no file")
-        .trim()
-        .to_string();
-    // let f = "3,4,3,1,2";
-    let fish: Vec<i64> = f.split(",").map(|x| x.parse::<i64>().unwrap()).collect();
-    let mut i = 80;
-    let mut part1: Vec<i64> = day(fish.clone());
-    while i > 1 {
-        part1 = day(part1);
-        i -= 1;
-    }
-    println!("{}", part1.len());
+  let f = fs::read_to_string("d6.txt")
+    .expect("no file")
+    .trim()
+    .to_string();
+  // let f = "3,4,3,1,2";
+  let fish: Vec<i64> = f.split(",").map(|x| x.parse::<i64>().unwrap()).collect();
+  let mut i = 80;
+  let mut part1: Vec<i64> = day(fish.clone());
+  while i > 1 {
+    part1 = day(part1);
+    i -= 1;
+  }
+  println!("{}", part1.len());
 
-    let mut i = 256;
-    let mut m = to_hm(fish.clone());
-    while i > 0 {
-        m = day_better(m);
-        i -= 1;
-    }
-    let part2: i64 = m.values().sum();
-    println!("{}", part2);
+  let mut i = 256;
+  let mut m = to_hm(fish.clone());
+  while i > 0 {
+    m = day_better(m);
+    i -= 1;
+  }
+  let part2: i64 = m.values().sum();
+  println!("{}", part2);
 }
 // for part1
 fn day(fish: Vec<i64>) -> Vec<i64> {
-    let mut new_gen = Vec::new();
-    for fish in fish {
-        if fish == 0 {
-            new_gen.push(6);
-            new_gen.push(8);
-        } else {
-            new_gen.push(fish - 1);
-        }
+  let mut new_gen = Vec::new();
+  for fish in fish {
+    if fish == 0 {
+      new_gen.push(6);
+      new_gen.push(8);
+    } else {
+      new_gen.push(fish - 1);
     }
-    new_gen
+  }
+  new_gen
 }
 // for part2
 fn to_hm(fish: Vec<i64>) -> HashMap<i64, i64> {
-    let mut m = HashMap::new();
-    for fish in fish {
-        *m.entry(fish).or_insert(0) += 1;
-    }
-    m
+  let mut m = HashMap::new();
+  for fish in fish {
+    *m.entry(fish).or_insert(0) += 1;
+  }
+  m
 }
 fn day_better(m: HashMap<i64, i64>) -> HashMap<i64, i64> {
-    let mut m2 = HashMap::new();
-    for (fish, count) in &m {
-        if *fish != 0 {
-            *m2.entry(*fish - 1).or_insert(0) += *count;
-        }
+  let mut m2 = HashMap::new();
+  for (fish, count) in &m {
+    if *fish != 0 {
+      *m2.entry(*fish - 1).or_insert(0) += *count;
     }
-    let zeros = m.get(&0).get_or_insert(&0).clone();
-    *m2.entry(6).or_insert(0) += zeros;
-    *m2.entry(8).or_insert(0) += zeros;
-    m2
+  }
+  let zeros = m.get(&0).get_or_insert(&0).clone();
+  *m2.entry(6).or_insert(0) += zeros;
+  *m2.entry(8).or_insert(0) += zeros;
+  m2
 }
