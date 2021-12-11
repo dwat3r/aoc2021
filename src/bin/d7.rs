9c021d3386c0f49eae401f54dfff8b771f6010d4
dwat3r <hechtloliver@gmail.com>
2021-12-11 15:42:24 +0100

    formatting

--- src/bin/d7.rs
+++ src/bin/d7.rs
@@ -1,42 +1,42 @@
 use std::fs;
 
 fn main() {
-    let f = fs::read_to_string("d7.txt")
-        .expect("no file")
-        .trim()
-        .to_string();
-    // let f = "16,1,2,0,4,2,7,1,2,14";
-    let input: Vec<i32> = f.split(",").map(|x| x.parse::<i32>().unwrap()).collect();
-    println!("{:?}", input);
-    let part1: i32 = calc_part1(&input);
-    println!("{}", part1);
-    let part2: i32 = calc_part2(&input);
-    println!("{}", part2);
+  let f = fs::read_to_string("d7.txt")
+    .expect("no file")
+    .trim()
+    .to_string();
+  // let f = "16,1,2,0,4,2,7,1,2,14";
+  let input: Vec<i32> = f.split(",").map(|x| x.parse::<i32>().unwrap()).collect();
+  println!("{:?}", input);
+  let part1: i32 = calc_part1(&input);
+  println!("{}", part1);
+  let part2: i32 = calc_part2(&input);
+  println!("{}", part2);
 }
 
 fn calc_part1(v: &Vec<i32>) -> i32 {
-    let min = *v.iter().min().expect("");
-    let max = *v.iter().max().expect("");
-    let v2: Vec<i32> = (min..(max + 1))
-        .map(|val| v.iter().map(|x| (x - val).abs()).sum())
-        .collect();
-    println!("{:?}", v2);
-    *v2.iter().min().expect("")
+  let min = *v.iter().min().expect("");
+  let max = *v.iter().max().expect("");
+  let v2: Vec<i32> = (min..(max + 1))
+    .map(|val| v.iter().map(|x| (x - val).abs()).sum())
+    .collect();
+  println!("{:?}", v2);
+  *v2.iter().min().expect("")
 }
 
 fn calc_part2(v: &Vec<i32>) -> i32 {
-    let min = *v.iter().min().expect("");
-    let max = *v.iter().max().expect("");
-    let v2: Vec<i32> = (min..(max + 1))
-        .map(|val| {
-            v.iter()
-                .map(|x| {
-                    let dist = (x - val).abs();
-                    dist * (dist + 1) / 2
-                })
-                .sum()
+  let min = *v.iter().min().expect("");
+  let max = *v.iter().max().expect("");
+  let v2: Vec<i32> = (min..(max + 1))
+    .map(|val| {
+      v.iter()
+        .map(|x| {
+          let dist = (x - val).abs();
+          dist * (dist + 1) / 2
         })
-        .collect();
-    println!("{:?}", v2);
-    *v2.iter().min().expect("")
+        .sum()
+    })
+    .collect();
+  println!("{:?}", v2);
+  *v2.iter().min().expect("")
 }
