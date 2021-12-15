ae469ec1fd02dacaf9eb3049b28b6cd12b9de8e6
dwat3r <hechtloliver@gmail.com>
2021-12-15 22:27:18 +0100

    d4 p2

--- src/bin/d4.rs
+++ src/bin/d4.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
 struct Input {
   numbers: Vec<u32>,
   boards: Vec<Vec<Vec<(u32, bool)>>>,
@@ -8,7 +8,7 @@ struct Input {
 
 fn main() {
   let f = fs::read_to_string("d4.txt").expect("no file");
-  //   let f = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+  // let f = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
 
   // 22 13 17 11  0
   //  8  2 23  4 24
@@ -30,10 +30,16 @@ fn main() {
 
   let mut input = parse(&f.to_string());
   // println!("{:#?}", input);
-  let part1 = draw_numbers(&mut input);
+  let part1 = draw_numbers(&mut input, false);
   println!("{}", part1);
+
+  let mut input = parse(&f.to_string());
+  println!("{}", input.numbers.len());
+  // println!("{:#?}", input);
+  let part2 = draw_numbers(&mut input, true);
+  println!("{}", part2);
 }
-fn draw_numbers(input: &mut Input) -> u32 {
+fn draw_numbers(input: &mut Input, last: bool) -> u32 {
   let mut ret: u32 = 0;
   for number in &input.numbers {
     for board in &mut input.boards {
@@ -45,8 +51,10 @@ fn draw_numbers(input: &mut Input) -> u32 {
         }
       }
     }
-    match check_winner(input) {
-      Some(board) => {
+    let inputc = input.clone();
+    let winners = check_winner(&inputc);
+    if winners.len() > 0 {
+      for board in winners.iter() {
         ret = board
           .iter()
           .flatten()
@@ -54,26 +62,39 @@ fn draw_numbers(input: &mut Input) -> u32 {
           .map(|n| n.0)
           .sum::<u32>()
           * number;
-        break;
+        if !last {
+          break;
+        } else {
+        }
       }
-      None => {
-        continue;
-      }
+      input.boards = input
+        .boards
+        .clone()
+        .into_iter()
+        .filter(|board| winners.iter().find(|wboard| ***wboard == *board).is_none())
+        .collect::<Vec<Vec<Vec<(u32, bool)>>>>();
     }
   }
   ret
 }
 
-fn check_winner(input: &Input) -> Option<&Vec<Vec<(u32, bool)>>> {
-  input.boards.iter().find(|board| {
-    let horiz = board.iter().any(|line| line.iter().all(|n| n.1));
-    let mut vert = vec![true; board.len()];
-    board.iter().enumerate().for_each(|(y, line)| {
-      line.iter().for_each(|n| vert[y] = vert[y] && n.1);
-    });
-    let vert = vert.iter().any(|x| *x);
-    horiz || vert
-  })
+fn check_winner(input: &Input) -> Vec<&Vec<Vec<(u32, bool)>>> {
+  input
+    .boards
+    .iter()
+    .filter(|board| {
+      let horiz = board.iter().any(|line| line.iter().all(|n| n.1));
+      let mut vert = vec![true; board.len()];
+      board.iter().for_each(|line| {
+        line
+          .iter()
+          .enumerate()
+          .for_each(|(y, n)| vert[y] = vert[y] && n.1);
+      });
+      let vert = vert.iter().any(|x| *x);
+      horiz || vert
+    })
+    .collect()
 }
 
 fn parse(f: &String) -> Input {
@@ -86,7 +107,8 @@ fn parse(f: &String) -> Input {
     .map(|x| x.trim().parse::<u32>().unwrap())
     .collect::<Vec<u32>>();
 
-  let boards = f.trim()
+  let boards = f
+    .trim()
     .split("\n\n")
     .skip(1)
     .map(|board| {
