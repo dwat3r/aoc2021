34322c487cf0060a141561fed9ea331986c2557b
dwat3r <hechtloliver@gmail.com>
2022-08-17 15:03:42 +0200

    update

--- src/bin/d12.rs
+++ src/bin/d12.rs
@@ -2,4 +2,15 @@
 
 fn main() {
   // let f = fs::read_to_string("d12.txt").expect("no file");
+  let f = "start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end";
+
+  let input = f.lines().map(|line| {
+    let line
+  })
 }
