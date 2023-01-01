eea2a80151b06f96a94b8594149fc1b237386e56
dwat3r <hechtloliver@gmail.com>
2023-01-01 22:13:41 +0100

    d12 wip

--- src/bin/d12.rs
+++ src/bin/d12.rs
@@ -1,8 +1,11 @@
 // use std::fs;
+use std::collections::HashMap;
+
+type Input<'a> = HashMap<&'a str, Vec<&'a str>>;
 
 fn main() {
-  // let f = fs::read_to_string("d12.txt").expect("no file");
-  let f = "start-A
+    // let f = fs::read_to_string("d12.txt").expect("no file");
+    let f = "start-A
 start-b
 A-c
 A-b
@@ -10,7 +13,27 @@ b-d
 A-end
 b-end";
 
-  let input = f.lines().map(|line| {
-    let line
-  })
+    let input = get_input(f);
+}
+
+fn get_input(f: &str) -> Input {
+    f.split_whitespace()
+        .map(|line| {
+            let r: Vec<&str> = line.split('-').collect();
+            (r[0], r[1])
+        })
+        .fold(HashMap::new(), |mut m: Input, (from, to)| {
+            let tos: Vec<&str> = if let Some(tos) = m.get_mut(from) {
+                tos.push(to);
+                tos.to_vec()
+            } else {
+                vec![to]
+            };
+            m.insert(from, tos);
+            m
+        })
+}
+
+fn list_paths(input: Input) -> Vec<Vec<&str>> {
+    input
 }
